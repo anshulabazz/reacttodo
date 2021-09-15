694bd47e6245eb11945565ca0a3dd90437cf3996
Anshul Sharma <9882843092as@gmail.com>
2021-09-15 17:51:55 +0530

    first commit

---
+++ server/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+module.exports = async () => {
+    try {
+        const connectionParams = {
+            useNewUrlParser: true,
+            
+            useUnifiedTopology: true,
+        };
+        await mongoose.connect(
+            "mongodb://localhost:27017/todo-apps",
+                
+            connectionParams
+        );
+        console.log("Connected to database.");
+    } catch (error) {
+        console.log("Could not connect to database.", error);
+    }
+};
